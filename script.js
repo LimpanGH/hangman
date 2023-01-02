3cd408a4f68b453af18ce1cd81b6d3d338c5d645
LimpanGH <Linusj555@gmail.com>
2023-01-02 10:49:34 +0100

    fully working

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ var programming_languages = [
 
 let answer = '';
 let maxWrong = 6;
-let misstakes = 0;
+let mistakes = 0;
 let guessed = [];
 let wordStatus = null;
 
@@ -27,11 +27,11 @@ function randomWord() {
 }
 
 function generateButtons() {
-    let buttonsHTML = 'abcdefghijklmnopqrstuvwxzy' .split('').map(letter =>
+    let buttonsHTML = 'abcdefghijklmnopqrstuvwxzy'.split('').map(letter =>
         `
         <button
-            class="btn btn-lg btn-primary m-2> 
-            id='` + letter + `
+            class="btn btn-lg btn-primary m-2" 
+            id='` + letter + `'
             onClick="handleGuess('` + letter + `')"
         >
             ` + letter + `
@@ -40,13 +40,67 @@ function generateButtons() {
     document.getElementById('keyboard').innerHTML = buttonsHTML;    
 }
 
+function handleGuess(chosenLetter) {
+    guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+    document.getElementById(chosenLetter).setAttribute('disabled', true);
+    
+    //alert(answer);
+
+    if (answer.indexOf(chosenLetter) >= 0) {
+        guessedWord();
+        checkIfGameWon();
+    } 
+    
+    else if (answer.indexOf (chosenLetter) === -1) {
+        mistakes++;
+        updateMistakes(); 
+        checkIfGameLost();
+        updateHangmanPicture();
+    }
+}
+
+function updateHangmanPicture() {
+    document.getElementById('hangmanPic').src = './images/' + mistakes + '.jpg';
+}
+
+function checkIfGameWon() {
+    if (wordStatus === answer) {
+        document.getElementById('keyboard').innerHTML = 'You Won!!!';
+    }
+}
+
+    function checkIfGameLost() {
+    if (mistakes === maxWrong) {
+        document.getElementById('wordSpotlight').innerHTML = 'The answer was: ' + answer;
+        document.getElementById('keyboard').innerHTML = 'You Lost!!!';
+    }
+}
+
 function guessedWord() {
     wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
     
     document.getElementById('wordSpotlight').innerHTML = wordStatus;
 }
 
-document.getElementById('maxWrong').innerHTML = maxWrong
+ function updateMistakes() { 
+    document.getElementById('mistakes').innerHTML = mistakes;
+ }
+
+ function reset() {
+    mistakes = 0;
+    guessed = [];
+    document.getElementById('hangmanPic').src = './images/0.jpg'; 
+
+    randomWord ();
+    guessedWord();
+    updateMistakes();
+    generateButtons();
+
+ }
+
+    document.getElementById('maxWrong').innerHTML = maxWrong
+
+
 randomWord();
 generateButtons();
 guessedWord();
